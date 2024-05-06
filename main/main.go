9b5857211c39676d03cb8bb39909ec13fce4ac83
pmperrin <pierremichel.perrin@gmail.com>
2024-05-06 22:50:18 +0200

    add translation and continue add check

--- main/main.go
+++ main/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
-	"github.com/pmperrin/examples"
 	"github.com/pmperrin/simpleI18n"
 )
 
@@ -12,42 +12,71 @@ import (
  * It initializes and runs the examples.
  */
 func main() {
-	/**
-	 * ExampleGoText test the usage of Go text.
-	 * See: https://github.com/leonelquinteros/gotext
-	 * result : the PO file is to long key and value are on different line
-	 */
-	examples.ExampleGoText()
+	// /**
+	//  * ExampleGoText test the usage of Go text.
+	//  * See: https://github.com/leonelquinteros/gotext
+	//  * result : the PO file is to long key and value are on different line
+	//  */
+	// examples.ExampleGoText()
 
-	/**
-	 * ExampleGo18n demonstrates the usage of 18n in Go.
-	 * See: "github.com/nicksnyder/go-i18n/v2/i18n"
-	 * result : the TAML or YAML ...  file is to long key and value are on different line
-	 */
-	examples.ExampleGo18n()
+	// /**
+	//  * ExampleGo18n demonstrates the usage of 18n in Go.
+	//  * See: "github.com/nicksnyder/go-i18n/v2/i18n"
+	//  * result : the TAML or YAML ...  file is to long key and value are on different line
+	//  */
+	// examples.ExampleGo18n()
 
-	/**
-	 * ExampleProperties reads from properties files.
-	 * See: "github.com/go-ini/ini"
-	 * result : this lib is build for init file and not for i18n
-	 */
-	examples.ExampleProperties()
+	// /**
+	//  * ExampleProperties reads from properties files.
+	//  * See: "github.com/go-ini/ini"
+	//  * result : this lib is build for init file and not for i18n
+	//  */
+	// examples.ExampleProperties()
 
-	/**
-	 * ExampleProperites2 basic read of properties files whitout lib
-	 */
-	examples.ExampleProperites2()
-	useLib()
+	// /**
+	//  * ExampleProperites2 basic read of properties files whitout lib
+	//  */
+	// examples.ExampleProperites2()
+	// useLib()
+	ExampleProperites2Server()
 }
 
 func useLib() {
 	fmt.Println(">> Starting lib")
 	//i18n := simpleI18n.SimpleI18n{Path: "./local", Filename: "messages"}
-	i18n, err := simpleI18n.InitI18n("./local", "messages")
+	i18n, err := simpleI18n.InitI18n("./local", "messages", "fr")
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	lang := i18n.GetLang("en")
-	fmt.Println(">" + lang.GetText("welcome.message"))
-	fmt.Println(">" + lang.GetTextWithParam("welcome.message.param", map[string]string{"name": "Toto"}))
+	lang, err := i18n.GetLang("ru")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	fmt.Println(">" + lang.GetText("signup.title") + "<br/>")
+	fmt.Println(">" + lang.GetTextWithParam("signup.button", map[string]string{"name": "Toto"}))
+
+}
+
+func ExampleProperites2Server() {
+
+	i18n, err := simpleI18n.InitI18n("./local", "messages", "fr")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Chargez les propriétés une seule fois
+		println("req / ")
+		lang, err := i18n.GetLang("ru")
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		fmt.Fprintf(w, "%s", lang.GetText("signup.title")+" - ")
+		fmt.Fprintf(w, "%s", lang.GetText("signup.button"))
+
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
--- simpleI18n/go-simple-i18n.go
+++ simpleI18n/go-simple-i18n.go
@@ -19,46 +19,49 @@ const (
 )
 
 type SimpleI18n struct {
-	Path     string
-	Filename string
-	langMap  map[string]LocalFile
+	Path        string
+	Filename    string
+	DefaultLang string
+	langMap     map[string]LocalFile
 }
 
 type LocalFile struct {
 	contante map[string]string
 }
 
-func InitI18n(path string, filename string) (SimpleI18n, error) {
+func InitI18n(path string, filename string, defaultLang string) (SimpleI18n, error) {
+	fmt.Println("Loading")
 	i18n := SimpleI18n{
-		Path:     path,
-		Filename: filename,
+		Path:        path,
+		Filename:    filename,
+		DefaultLang: defaultLang,
 	}
 	langList, err := i18n.findLanguageFile()
 	if err != nil {
 		return i18n, err
 	}
 	if len(langList) == 0 {
-		return i18n, errors.New("No language file found")
+		return i18n, errors.New("no language file found")
 	}
 
 	mu.Lock()
 	langMap, mapErr := i18n.loadLangMap(langList)
 	mu.Unlock()
 	i18n.langMap = langMap
-	fmt.Println(i18n.langMap)
 	return i18n, mapErr
 }
 
-// GetLangInstance returns the LocalFile instance for the specified language.
-//
-// Parameters:
-//   - lang (string): The language code for which the LocalFile instance is required.
-//
-// Returns:
-//   - (LocalFile): The LocalFile instance for the specified language.
-func (i SimpleI18n) GetLang(lang string) LocalFile {
-	fmt.Println(i.langMap)
-	return i.langMap[lang]
+func (i SimpleI18n) GetLang(lang string) (LocalFile, error) {
+	if i.langMap == nil {
+		return LocalFile{}, errors.New("no language file found during initialization")
+	}
+	if _, ok := i.langMap[lang]; !ok {
+		if _, ook := i.langMap[i.DefaultLang]; !ook {
+			return LocalFile{}, errors.New("no default language file found")
+		}
+		return i.langMap[i.DefaultLang], nil
+	}
+	return i.langMap[lang], nil
 }
 
 // GetText returns the value of the specified key from the localized content.
@@ -96,13 +99,11 @@ func (t LocalFile) GetTextWithParam(key string, param map[string]string) string 
 func (i SimpleI18n) findLanguageFile() ([]string, error) {
 	files, err := os.ReadDir(i.Path)
 	if err != nil {
-		fmt.Println("Error reading directory:", err)
 		return nil, err
 	}
 
 	langList := []string{}
 	for _, file := range files {
-		fmt.Println(file.Name())
 
 		if strings.Contains(file.Name(), ".properties") && strings.Contains(file.Name(), "_") {
 			strArray := strings.Split(file.Name(), "_")
@@ -115,10 +116,22 @@ func (i SimpleI18n) findLanguageFile() ([]string, error) {
 	return langList, nil
 }
 
+// loadLangMap loads the language files and returns a map of LocalFile instances.
+//
+// It takes a slice of strings containing the names of the found language files.
+// It returns a map of LocalFile instances, where the keys are the language codes and the values are the corresponding LocalFile instances.
+//
+// If an error occurs while reading the directory or loading the language file, it returns nil and the error.
+//
+// Parameters:
+//   - langList ([]string): A slice of strings containing the names of the found language files.
+//
+// Returns:
+//   - (map[string]LocalFile, error): A map of LocalFile instances and an error if any.
 func (i SimpleI18n) loadLangMap(langList []string) (map[string]LocalFile, error) {
 	tmpMap := make(map[string]LocalFile)
 	for _, lang := range langList {
-		fileContante, err := i.loadLangFile(lang)
+		fileContante, err := i.loadPropertiesContante(lang)
 		if err != nil {
 			return nil, err
 		}
@@ -126,13 +139,24 @@ func (i SimpleI18n) loadLangMap(langList []string) (map[string]LocalFile, error)
 		tmpMap[lang] = LocalFile{
 			contante: fileContante,
 		}
-		fmt.Println(lang + ": " + tmpMap[lang].contante["welcome.message"])
 	}
-	fmt.Println(tmpMap)
 	return tmpMap, nil
 }
 
-func (i SimpleI18n) loadLangFile(lang string) (map[string]string, error) {
+// loadPropertiesContante reads a language file and returns a map of key-value pairs.
+//
+// It takes a language code as a parameter and returns a map of strings where the keys are the keys from the properties file and the values are the corresponding values.
+//
+// The function opens the specified language file, reads its content line by line, and splits each line into a key-value pair. It then populates a map with these key-value pairs and returns it.
+//
+// If an error occurs while opening the file or reading its content, the function returns nil and the error.
+//
+// Parameters:
+//   - lang (string): The language code for which the language file is to be read.
+//
+// Returns:
+//   - (map[string]string, error): A map of strings containing the key-value pairs from the language file, and an error if any.
+func (i SimpleI18n) loadPropertiesContante(lang string) (map[string]string, error) {
 	props := make(map[string]string)
 	filePath := i.Path + "/" + i.Filename + "_" + lang + ".properties"
 	file, err := os.Open(filePath)
